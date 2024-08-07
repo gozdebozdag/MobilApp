b59bb49976fe1418b82eff80c7d158ef33a5d7ec
Sefa Yılmaz <105202814+sefaylmz187@users.noreply.github.com>
2024-08-07 11:31:03 +0300

    Update UnitOfWork.cs

--- MobilApp.DataAccess/Implementation/UnitOfWork.cs
+++ MobilApp.DataAccess/Implementation/UnitOfWork.cs
@@ -1,59 +1,40 @@
-using Microsoft.AspNetCore.Mvc;
-using MobilApp.API.Services;
-using MobilApp.DataAccess.Context;
-using MobilApp.Entities;
+﻿using MobilApp.DataAccess.Context;
 using MobilApp.Repository;
-using System.Linq.Expressions;
-using System.Security.Claims;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace MobilApp.API.Controllers
+namespace MobilApp.DataAccess.Implementation
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AuthController : Controller
+    public class UnitOfWork : IUnitOfWork
     {
-        private readonly MobilAppDbContext _context;
-        private readonly IConfiguration _configuration;
-        private readonly IUnitOfWork _unitOfWork;
-        readonly ITokenService _tokenService;
-
-        public AuthController(MobilAppDbContext context, IConfiguration configuration, IUnitOfWork unitOfWork, ITokenService tokenService)
+        private readonly MobilAppDbContext _carcontext;
+        public UnitOfWork(MobilAppDbContext carcontext)
         {
-            _unitOfWork = unitOfWork;
-            _context = context;
-            _configuration = configuration;
-            _tokenService = tokenService;
+            _carcontext = carcontext;
+            User = new UserRepository(_carcontext);
+            Product = new ProductRepository(_carcontext);
+            Group = new GroupRepository(_carcontext);
+            Category = new CategoryRepository(_carcontext);
+            Brand = new BrandRepository(_carcontext);
+            Customer = new CustomerRepository(_carcontext);
         }
-        [HttpPost, Route("login")]
-        public async Task<IActionResult> Login(string Eposta, string Password)
+
+        public IUserRepository User { get;private set; }
+        public IProductRepository Product { get; private set; }
+        public IGroupRepository Group { get; private set; }
+        public ICategoryRepository Category { get; private set; }
+        public IBrandRepository Brand { get; private set; }
+        public ICustomerRepository Customer { get; private set; }
+        public int Save()
         {
-            Expression<Func<User, bool>> predicate = x => x.Eposta == Eposta && x.Password == Password;
-
-            var user = _unitOfWork.User.Authenticate(predicate);
-            if (user == null)
-            {
-                return Unauthorized();
-            }
-            if (user.IsActive == false) { return Ok(new { Status = 0, message = "Kullanıcı hesabınız pasif durumdadır." }); }
-
-            var claims = new[]
-            {
-                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Sub, user.UserID.ToString()),
-                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Sid, user.NameSurname.ToString()),
-                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-            };
-
-            var accessToken = _tokenService.GenerateAccessToken(claims);
-            var refreshToken = _tokenService.GenerateRefreshToken();
-            user.LastToken = refreshToken;
-            user.TokenUseDate = DateTime.Now.AddDays(7);
-            _context.SaveChanges();
-            return Ok(new
-            {
-                Status = 1,
-                AccessToken = accessToken,
-                RefreshToken = refreshToken
-            });
+            return _carcontext.SaveChanges();
+        }
+        public void Dispose()
+        {
+            _carcontext.Dispose();
         }
     }
 }
